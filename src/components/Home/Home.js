5c5e343a7f8b4c0fea7bc380f9e3c60d0275495c
afifsylhet <afif.ahmed@yahoo.com>
2021-10-04 23:37:29 +0600

    Done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="d-flex align-items-center row px-2 py-4">
             <div className="px-5 col-lg-6">
                 <h1 className="text-primary">WellCome to Sun Shine</h1>
-                <h1>Learing Center</h1>
+                <h1>Learning Center</h1>
                 <p className="py-4">Sun Shine Learning Center is leading learning center in this area. Sun Shine learning center was establish to elemenate unemployment problem in this area. We have reach content and high profile teacher from different countries. So that people love us. If you want to change your current status and make a valuable assect of this country, please don't hasitate to start with us. </p>
 
                 <NavLink to="/undercons">
